fde320dc31a5ac79c9360110c6cfedafbdbb3bc0
Errai <erraineon@gmail.com>
2018-04-07 15:30:55 +0200

    add sqlite

---
+++ Tadmor/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tadmor.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+        {
+            public AppDbContext CreateDbContext(string[] args)
+            {
+                //ef cli tools will create the db in the project dir without this
+                Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+                var services = Program.GetServiceProvider();
+                return services.GetService<AppDbContext>();
+            }
+        }
+    }
+}
--- Tadmor/Program.cs
+++ Tadmor/Program.cs
@@ -2,9 +2,11 @@
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Tadmor.Data;
 using Tadmor.Services;
 
 namespace Tadmor
@@ -16,6 +18,8 @@ namespace Tadmor
             try
             {
                 var provider = GetServiceProvider();
+                var context = provider.GetService<AppDbContext>();
+                await context.Database.MigrateAsync();
                 var discord = provider.GetService<DiscordService>();
                 await discord.Start();
                 await Task.Delay(-1);
@@ -27,14 +31,14 @@ namespace Tadmor
             }
         }
 
-
-        private static ServiceProvider GetServiceProvider()
+        public static ServiceProvider GetServiceProvider()
         {
             var services = new ServiceCollection();
             var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             services
                 .Configure<DiscordOptions>(configuration.GetSection(nameof(DiscordOptions)))
                 .AddLogging(logger => logger.AddConsole())
+                .AddDbContext<AppDbContext>(builder => builder.UseSqlite(configuration.GetConnectionString("Main")))
                 .AddSingleton(new CommandService(new CommandServiceConfig {DefaultRunMode = RunMode.Async}))
                 .Scan(scan => scan
                     .AddTypes(typeof(DiscordSocketClient))
