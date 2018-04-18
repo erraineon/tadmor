b6c93c66367f1d3d7a303475c6d594c9ad144219
Errai <erraineon@gmail.com>
2018-04-18 21:58:07 +0200

    add .foodgore

---
+++ Tadmor/Modules/RandomPictureModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Tadmor.Services.Tumblr;
+
+namespace Tadmor.Modules
+{
+    public class RandomPictureModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly TumblrService _tumblr;
+
+        public RandomPictureModule(TumblrService tumblr)
+        {
+            _tumblr = tumblr;
+        }
+
+        [Command("foodgore")]
+        public async Task FoodGore()
+        {
+            var imageUrl = await _tumblr.GetRandomPost("someoneatethis");
+            var embed = new EmbedBuilder().WithImageUrl(imageUrl).Build();
+            await ReplyAsync(string.Empty, embed: embed);
+        }
+    }
+}
---
+++ Tadmor/Services/Tumblr/TumblrOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Tadmor.Services.Tumblr
+{
+    public class TumblrOptions
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+    }
+}
