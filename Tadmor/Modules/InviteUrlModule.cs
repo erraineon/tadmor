3d2de02f3445e479650e61e089a339100b1e3eec
Errai <erraineon@gmail.com>
2018-07-16 00:10:10 +0200

    add .inviteurl

---
+++ Tadmor/Modules/InviteUrlModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using Humanizer;
+
+namespace Tadmor.Modules
+{
+    [RequireBotPermission(ChannelPermission.CreateInstantInvite)]
+    [RequireOwner]
+    public class InviteUrlModule : ModuleBase<ICommandContext>
+    {
+        [Command("inviteurl")]
+        public async Task CreateInviteUrl(params string[] words)
+        {
+            await ReplyAsync($"searching for {words.Humanize()}");
+            var channel = (SocketGuildChannel) Context.Channel;
+            RestInviteMetadata invite;
+            while (true)
+            {
+                invite = await channel.CreateInviteAsync(null, isUnique: true);
+                if (words.Any(w => invite.Url.IndexOf(w, StringComparison.OrdinalIgnoreCase) >= 0)) break;
+                await invite.DeleteAsync();
+            }
+
+            await ReplyAsync(invite.Url);
+        }
+    }
+}
