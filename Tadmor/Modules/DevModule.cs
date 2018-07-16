d8168387dab8d7a08545702000bf5e5b8e2ce759
Errai <erraineon@gmail.com>
2018-07-16 15:46:27 +0200

    add .prune

--- Tadmor/Modules/DevModule.cs
+++ Tadmor/Modules/DevModule.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
 using Humanizer;
 
 namespace Tadmor.Modules
@@ -31,5 +35,31 @@ namespace Tadmor.Modules
 
         [Command("say")]
         public Task Say([Remainder] string message) => ReplyAsync(message);
+
+        [RequireBotPermission(ChannelPermission.CreateInstantInvite)]
+        [Command("inviteurl")]
+        public async Task CreateInviteUrl(params string[] words)
+        {
+            var options = new RequestOptions { RetryMode = RetryMode.RetryRatelimit };
+            await ReplyAsync($"searching for {words.Humanize()}");
+            var channel = (ITextChannel)Context.Channel;
+            IInviteMetadata invite;
+            while (true)
+            {
+                invite = await channel.CreateInviteAsync(null, isUnique: true, options: options);
+                if (words.Any(w => invite.Url.IndexOf(w, StringComparison.OrdinalIgnoreCase) >= 0)) break;
+                await invite.DeleteAsync(options);
+            }
+
+            await ReplyAsync(invite.Url);
+        }
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [Command("prune")]
+        public async Task Prune(int count)
+        {
+            var channel = (ITextChannel)Context.Channel;
+            var messages = await channel.GetMessagesAsync(count).FlattenAsync();
+            await channel.DeleteMessagesAsync(messages);
+        }
     }
 }
