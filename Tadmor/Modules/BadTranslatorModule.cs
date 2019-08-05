bd75045c39e3747dd46eab6b4263c59e370ce39d
Errai <erraineon@gmail.com>
2019-08-05 16:44:44 -0400

    add .tr command

---
+++ Tadmor/Modules/BadTranslatorModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Tadmor.Services.Yandex;
+
+namespace Tadmor.Modules
+{
+    [Summary("bad translator")]
+    public class BadTranslatorModule : ModuleBase<ICommandContext>
+    {
+        private readonly YandexService _yandex;
+
+        public BadTranslatorModule(YandexService yandex)
+        {
+            _yandex = yandex;
+        }
+
+        [Summary("gives a bad translation of a message")]
+        [Command("tr")]
+        public async Task BadTranslate([Remainder]string input)
+        {
+            if (input.Length > 140) throw new Exception("no more than 140 characters");
+            var badTranslation = await _yandex.BadTranslate(input);
+            await ReplyAsync(badTranslation);
+        }
+    }
+}
---
+++ Tadmor/Services/Yandex/YandexService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.Extensions.Options;
+using MoreLinq;
+using YandexTranslateCSharpSdk;
+
+namespace Tadmor.Services.Yandex
+{
+    public class YandexService
+    {
+        private readonly YandexTranslateSdk _yandex = new YandexTranslateSdk();
+        private List<string> _languages;
+
+        public YandexService(IOptions<YandexOptions> options)
+        {
+            _yandex.ApiKey = options.Value.ApiKey;
+        }
+
+        public async Task<string> BadTranslate(string input)
+        {
+            await EnsureLanguagesLoaded();
+            var languagesChain = new[] {"en"}
+                .Concat(new[] {"zh", "ja"}.Concat(_languages.RandomSubset(12)).Distinct())
+                .Concat(new[] {"en"}).ToList();
+            for (var index = 1; index < languagesChain.Count; index++)
+            {
+                var currentLanguage = languagesChain[index - 1];
+                var nextLanguage = languagesChain[index];
+                input = await _yandex.TranslateText(input, $"{currentLanguage}-{nextLanguage}");
+            }
+
+            return input;
+        }
+
+        private async Task EnsureLanguagesLoaded()
+        {
+            if (_languages == null) _languages = await _yandex.GetLanguages();
+        }
+    }
+
+    public class YandexOptions
+    {
+        public string ApiKey { get; set; }
+    }
+}
