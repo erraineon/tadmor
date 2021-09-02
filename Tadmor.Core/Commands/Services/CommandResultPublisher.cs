6872f02fb87f72c7a5bb5e59d7ba1da7a0f31a1c
Errai <erraineon@gmail.com>
2021-09-01 23:22:28 -0400

    split long messages

--- Tadmor.Core/Commands/Services/CommandResultPublisher.cs
+++ Tadmor.Core/Commands/Services/CommandResultPublisher.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
@@ -20,32 +22,54 @@ namespace Tadmor.Core.Commands.Services
                     commandContext.Channel.Id,
                     commandContext.Guild.Id)
                 : default;
-            switch (result)
+
+            var textsToSend = SplitLongText(result).ToList();
+            foreach (var textToSend in textsToSend)
             {
-                case CommandResult commandResult:
-                    var replyToOrNull = commandResult.Reply ? replyTo : default;
-                    if (commandResult.File is { } file)
-                        await commandContext.Channel.SendFileAsync(
-                            new MemoryStream(file.Data),
-                            file.FileName,
-                            commandResult.Reason,
-                            messageReference: replyToOrNull);
-                    else
-                        await commandContext.Channel.SendMessageAsync(
-                            commandResult.Reason,
-                            messageReference:
-                            replyToOrNull);
-                    break;
-                case RuntimeResult runtimeResult:
-                    await commandContext.Channel.SendMessageAsync(runtimeResult.Reason);
-                    break;
-                case ExecuteResult {Exception: ModuleException e}:
-                    await commandContext.Channel.SendMessageAsync(e.Message, messageReference: replyTo);
-                    break;
-                case ExecuteResult {Exception: {}}:
-                    await commandContext.Channel.SendMessageAsync("TADMOR IS DEAD", messageReference: replyTo);
-                    break;
+                switch (result)
+                {
+                    case CommandResult commandResult:
+                        var replyToOrNull = commandResult.Reply ? replyTo : default;
+                        if (commandResult.File is { } file)
+                            await commandContext.Channel.SendFileAsync(
+                                new MemoryStream(file.Data),
+                                file.FileName,
+                                textToSend,
+                                messageReference: replyToOrNull);
+                        else
+                            await commandContext.Channel.SendMessageAsync(
+                                textToSend,
+                                messageReference:
+                                replyToOrNull);
+                        break;
+                    case RuntimeResult:
+                        await commandContext.Channel.SendMessageAsync(textToSend);
+                        break;
+                    case ExecuteResult { Exception: ModuleException e }:
+                        await commandContext.Channel.SendMessageAsync(e.Message, messageReference: replyTo);
+                        break;
+                    case ExecuteResult { Exception: { } }:
+                        await commandContext.Channel.SendMessageAsync("TADMOR IS DEAD", messageReference: replyTo);
+                        break;
+                }
             }
+        }
+
+        private static IEnumerable<string> SplitLongText(IResult? result)
+        {
+            // splits the string based off the last found line break, if any, to fit the character limit
+            var textToSend = result?.ErrorReason ?? string.Empty;
+            var index = 0;
+            const int maxMessageLength = 2000;
+            while (textToSend.Length > maxMessageLength + index)
+            {
+                var lastLineBreakIndex = textToSend.LastIndexOf('\n', index + maxMessageLength);
+                if (lastLineBreakIndex == -1) lastLineBreakIndex = maxMessageLength;
+                yield return textToSend[index..lastLineBreakIndex];
+                index += lastLineBreakIndex;
+            }
+
+            yield return textToSend[index..];
         }
     }
 }
